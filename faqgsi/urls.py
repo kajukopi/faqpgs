ef6683d357f14f87dabeed07797508d28379ded4
Kaju Kopi <kajukopi@gmail.com>
2021-07-27 23:07:51 +0700

    update 11.07

--- faqgsi/urls.py
+++ faqgsi/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('', include('faq.urls')),
     path('admin/', admin.site.urls),
 ]
